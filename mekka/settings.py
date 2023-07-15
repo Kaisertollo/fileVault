05db007c29308a4b58a3689e7f811c1562dad79b
Your Name <you@example.com>
2023-07-15 21:14:32 +0000

    fff

--- mekka/settings.py
+++ mekka/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-*_zvqe0$q0@$jw@cdr$)!s*kb&gfu=j!r(o5sp#e@q5f^(#kl=
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-
-
-
+STATIC_ROOT = "/home/tollo/mekka/static"
 # Application definition
 
 INSTALLED_APPS = [
@@ -118,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
