568952b46190fd982d562d55284810bc3d8df4cf
Your Name <you@example.com>
2023-08-13 07:32:11 +0000

    model change

--- facturations/models.py
+++ facturations/models.py
@@ -39,8 +39,6 @@ class Customer(models.Model):
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=50,unique=True)
     token = models.CharField(max_length=250)
-    # Add any other fields you need for the customer model
-
     def __str__(self):
         return self.name
 
